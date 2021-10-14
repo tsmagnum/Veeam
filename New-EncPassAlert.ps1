641ab21efa056e715d7f0acfef82d2e21437c58f
Federico Lillacci <65031947+tsmagnum@users.noreply.github.com>
2021-10-14 14:33:54 +0200

    Add files via upload
    Small changes in the code comments.

--- New-EncPassAlert.ps1
+++ New-EncPassAlert.ps1
@@ -1,13 +1,18 @@
 <#
 .SYNOPSIS
 This scripts sends an email containing the log messages related to a successful Veeam encryption password modification.
-The script must be attached to a 31700 event in the "Veeam Backup" as a task, with a "Start a program action" to run this PS1 script.
+The script must be attached to 31700 and 31600 events in the "Veeam Backup" as a task, with a "Start a program action" to run this PS1 script.
 The email settings are MANDATORY!
 
 .DESCRIPTION
 This scripts sends an email containing the log messages related to a successful Veeam encryption password modification.
-The script must be attached to a 31700 event in the "Veeam Backup" as a task, with a "Start a program action" to run this PS1 script.
+The script must be attached to 31700 and 31600 events in the "Veeam Backup" as a task, with a "Start a program action" to run this PS1 script.
 The email settings are MANDATORY!
+
+.NOTES
+You should configure the scheduled task in Windows to run ("Actions" tab) "powershell.exe" 
+with arguments "-executionpolicy bypass -file C:\<path to your script dir>\New-EncPassAlert.ps1"
+You should also configure the task to run hidden and whether a user is logged or not (on the "General" tab of the task).
 
 Author: Federico Lillacci - Coesione Srl - https://github.com/tsmagnum
 #>
